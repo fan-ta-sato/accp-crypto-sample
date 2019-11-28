2134d2ade2f4c4afb53c1d9282e6831a2c8456f4
fan-ta-sato <ta_sato@fancs.com>
2019-11-28 09:47:38 +0900

    Add print Cipher provider

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -1,10 +1,14 @@
 package com.example
 
+import javax.crypto.Cipher
 import com.example.crypto.Crypto
 
 object Main extends App {
   println(
     com.amazon.corretto.crypto.provider.AmazonCorrettoCryptoProvider.PROVIDER_NAME)
+  println(
+    Cipher.getInstance("AES/GCM/NoPadding").getProvider().getName()
+  )
 
   val data = "Ango ka suru yo!"
   val key = "1234567890123456"
